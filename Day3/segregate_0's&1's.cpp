c43bc9056cd85daeb92c4e39e88590fdbb133568
gokulasree <kit28.24bcb016@gmail.com>
2026-06-30 18:27:55 +0530

    Update segregate_0's&1's.cpp
    Refactored segregate0and1 function to use temporary arrays for better clarity and organization.

--- Day3/segregate_0's&1's.cpp
+++ Day3/segregate_0's&1's.cpp
@@ -1,18 +1,31 @@
 class Solution {
-    int segregate0and1(vector<int> &arr) {
+  public:
+    void segregate0and1(vector<int> &arr) {
         // code here
-        // Arrays.sort(arr);
-        int ind=0;
-        for(int i=0;i<arr.size();i++)
-        {
-            if(arr[i]==0)
-            {
-                int t=arr[i];
-                arr[i]=arr[ind];
-                arr[ind]=t;
-                ind++;
-            }        
-        }  
-        return ind;
+        int n=arr.size();
+        vector<int> temp1;
+        vector<int> temp2;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                temp1.push_back(arr[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                temp2.push_back(arr[i]);
+            }
+        }
+        int a=0;
+        for(int i=0;i<temp1.size();i++){
+            arr[a]=temp1[i];
+            a++;
+        }
+        for(int i=0;i<temp2.size();i++){
+            arr[a]=temp2[i];
+            a++;
+        }
     }
-}
+};
+// time complexity is O(n) 
+// space complexity is O(n) for storing the temporary arrays
