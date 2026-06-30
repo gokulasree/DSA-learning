53dc7a0078421264a169a277adf3a311b3688ce5
gokulasree <kit28.24bcb016@gmail.com>
2026-06-30 18:09:53 +0530

    Update and rename Number_swap.cpp to pointer.cpp

---
+++ Day1/pointer.cpp
@@ -0,0 +1,14 @@
+// time complexity is O(1)
+// space complexity is O(1)
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a=10;
+    int *p=&a;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+
+    return 0;
+}
